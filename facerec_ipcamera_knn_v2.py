c89674269b06dc12f40136bc94d4fd41c5995cb8
HeeJung Hong <48550633+HongHeeJung@users.noreply.github.com>
2020-03-21 15:27:15 +0900

    Update facerec_ipcamera_knn_v2.py

--- facerec_ipcamera_knn_v2.py
+++ facerec_ipcamera_knn_v2.py
@@ -1,36 +1,25 @@
 """
 This is an example of using the k-nearest-neighbors (KNN) algorithm for face recognition.
-
 When should I use this example?
 This example is useful when you wish to recognize a large set of known people,
 and make a prediction for an unknown person in a feasible computation time.
-
 Algorithm Description:
 The knn classifier is first trained on a set of labeled (known) faces and can then predict the person
 in a live stream by finding the k most similar faces (images with closet face-features under eucledian distance)
 in its training set, and performing a majority vote (possibly weighted) on their label.
-
 For example, if k=3, and the three closest face images to the given image in the training set are one image of Biden
 and two images of Obama, The result would be 'Obama'.
-
 * This implementation uses a weighted vote, such that the votes of closer-neighbors are weighted more heavily.
-
 Usage:
-
 1. Prepare a set of images of the known people you want to recognize. Organize the images in a single directory
    with a sub-directory for each known person.
-
 2. Then, call the 'train' function with the appropriate parameters. Make sure to pass in the 'model_save_path' if you
    want to save the model to disk so you can re-use the model without having to re-train it.
-
 3. Call 'predict' and pass in your trained model to recognize the people in a live video stream.
-
 NOTE: This example requires scikit-learn, opencv and numpy to be installed! You can install it with pip:
-
 $ pip3 install scikit-learn
 $ pip3 install numpy
 $ pip3 install opencv-contrib-python
-
 """
 
 import cv2
@@ -43,6 +32,7 @@ from PIL import Image, ImageDraw
 import face_recognition
 from face_recognition.face_recognition_cli import image_files_in_folder
 import numpy as np
+
 # FOR STORAGE
 try:
     from google.cloud import storage
@@ -68,7 +58,7 @@ db_app = firebase_admin.initialize_app(cred, {'databaseURL': db_url})
 alldata = db.reference()
 username = alldata.child("06수 8850/username").get()
 sr_buck = 'whoru-ed991.appspot.com'
-sr_app = firebase_admin.initialize_app(cred, {'storageBucket': sr_buck,}, name='storage')
+sr_app = firebase_admin.initialize_app(cred, {'storageBucket': sr_buck, }, name='storage')
 #########################################################
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'JPG'}
@@ -77,11 +67,8 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'JPG'}
 def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree', verbose=False):
     """
     Trains a k-nearest neighbors classifier for face recognition.
-
     :param train_dir: directory that contains a sub-directory for each known person, with its name.
-
      (View in source code to see train_dir example tree structure)
-
      Structure:
         <train_dir>/
         ├── <person1>/
@@ -92,7 +79,6 @@ def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree
         │   ├── <somename1>.jpeg
         │   └── <somename2>.jpeg
         └── ...
-
     :param model_save_path: (optional) path to save model on disk
     :param n_neighbors: (optional) number of neighbors to weigh in classification. Chosen automatically if not specified
     :param knn_algo: (optional) underlying data structure to support knn.default is ball_tree
@@ -115,7 +101,8 @@ def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree
             if len(face_bounding_boxes) != 1:
                 # If there are no people (or too many people) in a training image, skip the image.
                 if verbose:
-                    print("Image {} not suitable for training: {}".format(img_path, "Didn't find a face" if len(face_bounding_boxes) < 1 else "Found more than one face"))
+                    print("Image {} not suitable for training: {}".format(img_path, "Didn't find a face" if len(
+                        face_bounding_boxes) < 1 else "Found more than one face"))
             else:
                 # Add face encoding for current image to the training set
                 X.append(face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0])
@@ -142,7 +129,6 @@ def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree
 def predict(X_frame, knn_clf=None, model_path=None, distance_threshold=0.5):
     """
     Recognizes faces in given image using a trained KNN classifier
-
     :param X_frame: frame to do the prediction on.
     :param knn_clf: (optional) a knn classifier object. if not specified, model_save_path must be specified.
     :param model_path: (optional) path to a pickled knn classifier. if not specified, model_save_path must be knn_clf.
@@ -173,14 +159,14 @@ def predict(X_frame, knn_clf=None, model_path=None, distance_threshold=0.5):
     are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
 
     # Predict classes and remove classifications that aren't within the threshold
-    return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
+    return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in
+            zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
 
 
 def show_prediction_labels_on_image(frame, predictions):
     global cnt_face
     """
     Shows the face recognition results visually.
-
     :param frame: frame to show the predictions on
     :param predictions: results of the predict function
     :return opencv suited image to be fitting with cv2.imshow fucntion:
@@ -202,20 +188,18 @@ def show_prediction_labels_on_image(frame, predictions):
         name = name.encode("UTF-8")
         names = name.decode()
 
-
-############### name compare ######################################
+        ############### name compare ######################################
         if names == username:
             print("true")
             cnt_face += 1
             if cnt_face == 40:
-                alldata.child("06수 8850").update({'FLAG' : '1'})
+                alldata.child("06수 8850").update({'FLAG': '1'})
         else:
             if cnt_face < 40:
                 cnt_face == 0
             print("false")
-#################################################################
+        #################################################################
 
-        
         # Draw a label with a name below the face
         text_width, text_height = draw.textsize(name)
         draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
@@ -233,8 +217,11 @@ if __name__ == "__main__":
     ################### Call Image ######################
     bucket = storage.bucket(app=sr_app)
     blob = bucket.blob("WhoRU_target/{}.jpg".format(username))
+    user_path = "./knn_examples/train/{}".format(username)
+    if not os.path.isdir(user_path):
+        os.mkdir(user_path)
     img_url = blob.generate_signed_url(datetime.timedelta(seconds=300), method='GET')
-    urllib.request.urlretrieve(img_url, './knn_examples/train/{}/{}.jpg'.format(username,username))
+    urllib.request.urlretrieve(img_url, '{0}/{1}.jpg'.format(user_path, username))
     print("save")
     ######################################################
     print("Training KNN classifier...")
@@ -246,8 +233,7 @@ if __name__ == "__main__":
     # multiple cameras can be used with the format url = 'http://username:password@camera_ip:port'
     url = 'http://192.168.0.17:8081/'
     cap = cv2.VideoCapture(url)
-    
-    
+
     while 1 > 0:
         ret, frame = cap.read()
         if ret:
